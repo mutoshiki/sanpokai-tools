905fb041e55add802e6556f2428eda298df602a3
mutoshiki <xh26zjnd8y@privaterelay.appleid.com>
2026-05-05 00:32:28 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+window.SANPO_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBdgDzo6C7CG739XvqlD9RtKmiQvHuIXbY",
+  authDomain: "sanpokai-tool.firebaseapp.com",
+  databaseURL: "https://sanpokai-tool-default-rtdb.firebaseio.com",
+  projectId: "sanpokai-tool",
+  storageBucket: "sanpokai-tool.firebasestorage.app",
+  messagingSenderId: "79505558920",
+  appId: "1:79505558920:web:3f9a9a333fc77de7a7fe3d"
+};
